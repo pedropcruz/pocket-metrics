3f9504a580b607aca781f299e73325d1cbe5cd5d
Pedro Cruz <pedropcruz@outlook.pt>
2024-08-16 16:48:42 +0100

    fix(): components styles on mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,14 @@ export default function App() {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center w-full bg-slate-100">
-      <h1 className="text-4xl font-bold mb-12">Pocket Metrics</h1>
-      <main className="grid md:grid-cols-2 gap-8 w-full max-w-4xl mx-auto items-center">
+    <div className="relative flex flex-col items-center justify-center w-full min-h-screen">
+      <h1 className="text-4xl font-bold my-12 sm:mt-0">Pocket Metrics</h1>
+      <main className="grid md:grid-cols-2 gap-8 w-full container max-w-4xl mx-auto items-center">
         <SalaryForm onSubmit={handleSubmit} />
         <BudgetChart salary={salary} budgetRule={budgetRule} />
       </main>
 
-      <footer className="fixed bottom-0 left-0 z-20 w-full p-4  border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
+      <footer className="relative mt-12 sm:mt-0 sm:absolute bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
